1cac406e03e717d0565ee2983a9e267aa55aaa97
Daniel Bergmann <dba222@outlook.com>
2021-02-16 12:17:39 +0000

    made final touch

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,7 +36,7 @@ if (!spaceId || !accessToken) {
 
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Contentful starter",
+    title: "Gatsby Contentful tester",
   },
   pathPrefix: "/gatsby-contentful-starter",
   plugins: [
